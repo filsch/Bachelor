c3a9964ed596e4efbe47d146c16b16a377f038de
filsch <fschjerven@gmail.com>
2017-02-09 15:38:43 +0100

    La til covariansfunksjon pdf om prior

--- some_doodling.R
+++ some_doodling.R
@@ -8,12 +8,24 @@ square_mapping = interp(x = original_mapping$x, y = original_mapping$y, z = orig
                         yo = seq(1,max(original_mapping$y),length.out = 50))
 square_mapping$x = seq(50); square_mapping$y = seq(50)
 
+#Constructing isotropic correlation matrix. Now using Exponential
+corr_matrix <- function(pred_points, sample_points, range){
+  ncolumns = dim(pred_points)[1]; nrows = dim(sample_points)[1]
+  
+  corr_matrix = matrix(numeric(nrows*ncolumns),nrow=nrows)
+  
+  for (i in 1:ncolumns){
+    corr_matrix[,i] = sqrt( rowSums((sample_points - matrix(numeric(2*nrows) + 1,ncol=2)%*%diag(pred_points[i,]))^2) )
+  }
+  return( Exponential(corr_matrix, range = range) )
+}
+
 prior_field = expand.grid(seq(10), seq(10))
 
-prior ranges; prior variance; prior field; numeric approximation integrat;
-exponential covariance func; matern?; squared exp?; different polynomial trends?;
+#prior ranges; prior variance; prior field; numeric approximation integrat;
+#exponential covariance func; matern?; squared exp?; different polynomial trends?;
 
-krigerestimate of posterior
+#krigerestimate of posterior
 
-different designs
-multivariate normal for everything!
+#different designs
+#multivariate normal for everything!

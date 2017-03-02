73027d876959dc9ec0f29d9c8a7c11f0e5b3bcfb
filsch <fschjerven@gmail.com>
2017-03-02 12:15:16 +0100

    La til siste funksjoner. Mangler finsjekk

--- myglm_lite.R
+++ myglm_lite.R
@@ -1,4 +1,5 @@
 #Auxilliary functions for bachelor-thesis
+library(fields)
 
 #Constructing square map with adapted axes 
 squareMap <- function(map, grid_size){
@@ -9,8 +10,8 @@ squareMap <- function(map, grid_size){
   return(square_map)
 }
 
-#Constructs an isotropic correlation matrix based on the Exponential correlation function 
-correlationMatrix <- function(sample_points, prediction_points, range){
+#Constructs an isotropic correlation matrix based on the Exponential and Matern correlation function 
+correlationMatrix <- function(sample_points, prediction_points, range, correlation_function = 0, v=1.5){
   ncolumns = dim(prediction_points)[1]; nrows = dim(sample_points)[1]
   
   corr_matrix = matrix(numeric(nrows*ncolumns),nrow=nrows, ncol=ncolumns)
@@ -20,30 +21,45 @@ correlationMatrix <- function(sample_points, prediction_points, range){
   for (i in 1:ncolumns){
     corr_matrix[,i] = sqrt( colSums((t(sample_points) - (zero_matrix + prediction_points[i,]))^2))
   }
+  if (correlation_function == "exponential"){
   return( Exponential(corr_matrix, range = range) )
+  }
+  else if(correlation_function == "matern"){
+  return(Matern(corr_matrix, range = range, smoothness=v))
+  } else {cat('Choose either exponential or matern','\n'); return()}
 }
 
 #Samples n samples with position from the grid
 #(To check the values: z[ intersect(which(square_map$y==a),which(square_map$x==b))) ]
-gridSampler <- function(n, map, design, measurement_sigma = 0){
+gridSampler <- function(n, map, design, noise = 0){
   if (design == 'regular'){
+    xmax = max(map$x);   ymax = max(map$y);   xmin = min(map$x);   ymin = min(map$y)
+    
     #Designed to have a perfect regular grid, disregarding the actual n
-    xincrement = round( (max(map$x) - min(map$x))/round(sqrt(n)))
-    yincrement = round( (max(map$y) - min(map$y))/round(sqrt(n)))
+    xincrement = round( (xmax - xmin)/(round(sqrt(n)) - 1) )
+    yincrement = round( (ymax - ymin)/(round(sqrt(n)) - 1) )
     
-    samples = expand.grid(x = seq(min(map$x), max(map$x), xincrement), y= seq(min(map$y),max(map$y), yincrement))
-    samples$z = map$z[(samples$x-1)*50 + samples$y]
-    
+    samples = expand.grid(x = seq(xmin, xmax, xincrement), y = seq(ymin, ymax, yincrement))
+    samples$z = map$z[(samples$x-1)*xmax + samples$y]
   }
-  samples$z = samples$z + rnorm(length(samples$z),0, measurement_sigma)    
+  samples$z = samples$z + rnorm(length(samples$z),0, noise)    
   return(samples)
 }
-#(To check the values intersect(which(square_map$y==1),which(square_map$x==34)))
 
-
+#Constructs a "priorfield", in this instance using a uniform prior for range and gamma for GRF variance
+priorField <- function(size_sigma, size_range, alpha=5, beta=0.5){
+  #Centre mean, with some (theoretical) std deviation increments from the theoretical mean
+  increment = 2/floor(size_sigma/2)
+  values = round(seq(-floor(size_sigma/2),-floor(size_sigma/2) + size_sigma - 1)*increment*sqrt(alpha)/beta + alpha/beta)
+  lower = c(0, values[1:length(values) - 1] + diff(values)/2); upper = c(values[2:length(values)] - diff(values)/2, Inf)
+  
+  prior_field = expand.grid(sigma2=values, range = seq(1, size_range))
+  prior_field$prob = rep(1/size_range * (pgamma(upper, alpha,beta) - pgamma(lower,alpha,beta)), times = size_range)
+  return(prior_field)
+}
 
 #Performs GLS estimation of the samples
-glmLite <- function(trend, data){
+glmLite <- function(trend, data, correlation_matrix){
   if (trend == 'quadratic'){
     formula = z ~ x*y + I(x^2) + I(y^2)
   }
@@ -59,8 +75,53 @@ glmLite <- function(trend, data){
   Y  = model.response(mf);
   
   #Performing GLS 
-  W = solve(corr_matrix);
+  W = solve(correlation_matrix);
   covariance = solve(t(X)%*%W%*%X);
   beta = covariance%*%t(X)%*%W%*%Y;
   return(list(coefficients = beta, covar = covariance, formula = formula, X = X, terms = attr(mf, "terms")))
+}
+
+# glmLiteTestFunction <- function(trend,data, correlation_matrix){
+#   tranformation_matrix = t(chol(correlation_matrix))
+#   if (trend == 'quadratic'){
+#     formula = z ~ x*y + I(x^2) + I(y^2)
+#   }
+#   else if (trend == 'linear'){
+#     formula = z ~ x + y 
+#   }
+#   else if (trend == 'simple'){
+#     formula = z ~ 1
+#   }
+#   else {cat('Choose either simple, linear or quadratic trend','\n'); return()}
+#   
+#   myglm = glmLite(trend,data,correlation_matrix)
+#   data$z = data$z %*%
+#   exactglm = glm(formula, )
+# }
+
+posteriorDistribution <- function(size_predictionx, size_predictiony, correlation_function = 'exponential', glm_est, GRFsigma2 = 1){
+  prediction_grid = expand.grid(x = seq(1,size_predictionx), y = seq(1,size_predictiony));
+  prediction_grid$z = numeric(2500)
+  
+  mf = model.frame(formula = glm_est$formula, data = prediction_grid)
+  Xprediction  = model.matrix(attr(mf, "terms"), data = mf)
+  Xobserved = glm_est$X
+  
+  prediction_grid$z <- NULL
+  prediction_pos = cbind(prediction_grid$x, prediction_grid$y)
+  
+  correlation_prediction_observed = correlationMatrix(cbind(samples$x,samples$y), prediction_pos, range = 5, correlation_function);
+  correlation_prediction = correlationMatrix(prediction_pos, prediction_pos, range = 5, correlation_function);
+  correlation_observed = correlationMatrix(cbind(samples$x,samples$y),cbind(samples$x,samples$y), range = 5, correlation_function);
+  
+  fitted_predictions = Xprediction%*%glm_est$coefficients;
+  covariance_predictions_observed = Xprediction%*%glm_est$covar%*%t(Xobserved);
+  
+  sigma21 = t(correlation_prediction_observed * GRFsigma2) + covariance_predictions_observed
+  sigma22 = correlation_observed * GRFsigma2;
+  sigma11 = correlation_prediction * GRFsigma2;
+  
+  #En eller annen feil her. Unøyaktig
+  prediction_grid$z = fitted_predictions - (sigma21)%*%solve(sigma22)%*%(samples$z - Xobserved%*%glm_object$coefficients)
+  return(prediction_grid)
 }
--- some_doodling.R
+++ some_doodling.R
@@ -1,4 +1,3 @@
-library(geoR); library(MASS); library(akima); library(fields); library(RSAGA)
 set.seed(4545)
 
 #TODO:
@@ -8,10 +7,6 @@ set.seed(4545)
 # - Koble prior med prediksjonen
 # - Undersøke hvorfor 'linear' og 'quadratic' gir tilsynelatende motsatt resultater
 
-
-
-
-
 #Adapting data from matrix form
 original_mapping = grid.to.xyz(t(volcano))
 image.plot(original_mapping)
@@ -20,59 +15,37 @@ map = squareMap(original_mapping, 50)
 
 #--------------------------------------------------------------------------------------------------------
 
-#Assigning prior values and estimating beta
-alpha = 5; beta = 0.5; measurement_sigma = 0.1;
-#Centre mean, 0.4*std indrement out from centre (but why? because)
-values = round(seq(-5,4)*0.4*sqrt(alpha)/beta + alpha/beta)
-lower = c(0, values[1:length(values) - 1] + diff(values)/2); upper = c(values[2:length(values)] - diff(values)/2, Inf)
-prior_field = expand.grid(sigma2=values, range = seq(1,10))
-prior_field$prob = rep(0.1*(pgamma(upper, alpha,beta) - pgamma(lower,alpha,beta)), times = 10)
-
+#Constructing priorfield
+prior = priorField(10,10) 
 
 #--------------------------------------------------------------------------------------------------------
 
 #Generating sample from grid by design
-samples = gridSampler(50, map,"regular")
+samples = gridSampler(10, map,"regular", noise = 0.1)
 
 #--------------------------------------------------------------------------------------------------------
 # Fitting trend w.r.t. covariance matrix and sample position
-correlation_sample = cbind(grid_samples$x,grid_samples$y)
+correlation_sample = cbind(samples$x,samples$y)
 
-glm_object = myglm_lite('linear', grid_samples, corr_matrix(correlation_sample, correlation_sample, 5))
+glm_object = glmLite('quadratic', samples,
+                     correlationMatrix(correlation_sample, correlation_sample,
+                                       range = 5, correlation_function = 'exponential'))
 
 #--------------------------------------------------------------------------------------------------------
 
 #Predicting the data with glm estimates of trend
-prediction_grid = expand.grid(x = seq(1,50), y = seq(1,50)); prediction_grid$z = numeric(2500)
-
-mf = model.frame(formula = glm_object$formula, data = prediction_grid)
-Xs0  = model.matrix(attr(mf, "terms"), data = mf)
-Xs = glm_object$X
-
-fitted_glm = Xs0%*%glm_object$coefficients
-prediction_pos= cbind(prediction_grid$x, prediction_grid$y)
-
-correlation_sample_and_pred = corr_matrix(sample_correlation_input, prediction_pos, range = 5);
-correlation_predictions = corr_matrix(prediction_pos, prediction_pos, range = 5);
-correlation_samples = corr_matrix(sample_correlation_input,sample_correlation_input, range = 5);
-
-covar_beta_Xs0_Xs = Xs0%*%glm_object$covar%*%t(Xs);
-
-#Using 1 as placeholder for variance sigmacorrelation_samplescorrelation_samples
-sigma21 = correlation_sample_and_pred*1 + covar_beta_Xs0_Xs;
-sigma22 = correlation_samples*1;
-sigma11 = correlation_predictions*1;
-
-#En eller annen mindre feil her - noe unøyaktig
-prediction_grid$z = fitted_glm - (sigma21)%*%solve(sigma22)%*%(grid_samples$z - Xs%*%glm_object$coefficients)
-image.plot(prediction_grid)
-
+posterior_distribution = posteriorDistribution(50,50,glm_est=glm_object)
+image.plot(posterior_distribution)
 
 #--------------------------------------------------------------------------------------------------------
+sigma2=1
+prediction_grid$z <- NULL
+prediction_grid = expand.grid(x = seq(1,50), y = seq(1,50))
+
 #KRIGING:
 #Computing the predictor:
-posterior_kriger = krige.conv(coords=cbind(grid_samples$x,grid_samples$y), data=grid_samples$z, locations = prediction_grid,
-                              krige=krige.control(type.krige= "OK", trend.d = "2nd", trend.l = "2nd", cov.model="exponential", cov.pars=c(sigma2, range)));
+posterior_kriger = krige.conv(coords=cbind(samples$x,samples$y), data=samples$z, locations = prediction_pos,
+                              krige=krige.control(type.krige= "OK", trend.d = "2nd", trend.l = "2nd", cov.model="exponential", cov.pars=c(1, 5)));
 #Plotting some results
 par(mfrow=c(2,2))
 plot_values = list(z=matrix(posterior_kriger$predict,nrow=50,ncol=50,byrow=FALSE), x = seq(0,50), y = seq(0,50))
@@ -81,7 +54,7 @@ image.plot(plot_values, main="Kriging predictions, no noise in datamodel", zlim=
 plot_values$z = matrix(sqrt(posterior_kriger$krige.var),nrow=50,ncol=50,byrow=FALSE) 
 image.plot(plot_values, main="Kriging std.dev., no noise on data Y", zlim=c(0,5))
 
-plot_values$z = square_mapping$z - matrix(posterior_kriger$predict,nrow=50,ncol=50,byrow=FALSE) 
+plot_values$z = map$z - matrix(posterior_kriger$predict,nrow=50,ncol=50,byrow=FALSE) 
 
 image.plot(plot_values, main="Kriging vs actual", zlim=c(-30, 30))
-plot(grid_samples$x,grid_samples$y, main="Observed points")
+plot(samples$x,samples$y, main="Observed points")
